8450fd9c5ad44610692aee59c23a9192f2f82281
Chris <13814772745@163.com>
2026-09-16 18:51:07 +0800

    提交代码Tool初版设计

--- src/agent_lab/tool/base.py
+++ src/agent_lab/tool/base.py
@@ -1,15 +1,33 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Any
+
+from pydantic import BaseModel
+
+from .response import ToolResponse
+
+
+class ToolParameter(BaseModel):
+    name: str
+    annotation: Any
+    description: str
+    required: bool = False
+    default: Any = None
 
 
 class Tool(ABC):
-
     def __init__(
             self,
             name: str,
             description: str,
-            parameters: dict[str, Any]
+            parameters: list[ToolParameter]
     ):
         self.name = name
         self.description = description
         self.parameters = parameters
+
+    @abstractmethod
+    def run(
+            self,
+            arguments: dict[str, Any],
+    ) -> ToolResponse:
+        pass
--- src/agent_lab/tool/response.py
+++ src/agent_lab/tool/response.py
@@ -1,6 +1,19 @@
+from enum import Enum
+from typing import Any, Optional
+
 from pydantic.dataclasses import dataclass
 
+class ToolStatus(Enum):
+    """工具运行状态"""
+    SUCCESS = "success" #
+    # PARTIAL = "partial" # 结果可用但存在折扣（截断、回退、部分失败）
+    ERROR = "error"
+@dataclass
+class ToolResponse:
+    status: ToolStatus
 
-# @dataclass
-# class ToolResponse:
-#
+    text: str
+
+    data: dict[str, Any]
+
+    # error_info: Optional[dict[str, Any]]

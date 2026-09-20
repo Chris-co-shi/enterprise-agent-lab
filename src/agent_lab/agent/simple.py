a89f03efed8ea7c0e9416573a278dc5e0a8df114
Chris-co-shi <christ.sxc@gmail.com>
2026-09-20 16:22:41 +0800

    submit  Agent

--- src/agent_lab/agent/simple.py
+++ src/agent_lab/agent/simple.py
@@ -1,3 +1,5 @@
+from core import MessageRole
+from ..core import Message
 from ..llm import LLMClient
 from ..tool.registry import ToolRegistry
 from .base import BaseAgent
@@ -9,18 +11,34 @@ class SimpleAgent(BaseAgent):
             self,
             name: str,
             llm: LLMClient,
-            tool_registry: ToolRegistry | None = None, # 因为 Agent 即使没有工具，也应该能退化成普通 LLM Agent。
+            tool_registry: ToolRegistry | None = None,  # 因为 Agent 即使没有工具，也应该能退化成普通 LLM Agent。
             system_prompt: str | None = None,
-            max_tool_iterations: int = 5 # 最大循环数量
+            max_tool_iterations: int = 5  # 最大循环数量
     ):
         super().__init__(
-            name = name,
+            name=name,
             llm=llm,
             system_prompt=system_prompt,
             tool_registry=tool_registry
         )
         self.max_tool_iterations = max_tool_iterations
 
-
     def run(self, input_text: str):
         pass
+
+    def _build_messages(self, input_text: str) -> list[Message]:
+        """构建消息列表"""
+        messages = []
+
+        Message(
+            role="system",
+            content=self.system_prompt,
+
+        )
+        messages.extend(self.get_history())
+
+        messages.append(Message(
+            role="user",
+            content=input_text
+        ))
+        return messages
